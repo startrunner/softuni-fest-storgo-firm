71bd84fac86a2fe4db9ea7b71dbfb874babd78d8
Alexander Ivanov <alexander_ivanov@mail.com>
2019-06-16 12:37:38 +0300

    home.js table shows data & date :)

--- source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/components/Home/Home.js
+++ source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/components/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import { Grid, Table, TableHeader, TableHeaderCell, TableBody, TableRow, TableCell, Label, Segment, Image } from "semantic-ui-react";
 import "./Home.css";
+const moment = require("moment");
+
+let parseUtcDate = function (utcString) {
+    let utcTime = moment.utc(utcString/*, "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"*/);
+    let localTime = utcTime.toDate();
+    //return localTime.toString();
+    return localTime.toLocaleDateString();//("ddd-Do-Mo-YYYY");
+}
 
 export class Home extends Component {
     constructor(props) {
@@ -24,26 +32,27 @@ export class Home extends Component {
                             <Segment className="home-grid-main" >
                                 <Table>
                                     <Table.Header>
-                                        <TableHeader>
-                                            <TableHeaderCell>Event Name</TableHeaderCell>
-                                            <TableHeaderCell>Date</TableHeaderCell>
-                                            <TableHeaderCell>League</TableHeaderCell>
-                                            <TableHeaderCell>Sport</TableHeaderCell>
-                                        </TableHeader>
+                                        <Table.Row>
+                                            <Table.HeaderCell>Event Name</Table.HeaderCell>
+                                            <Table.HeaderCell>Date</Table.HeaderCell>
+                                            <Table.HeaderCell>League</Table.HeaderCell>
+                                            <Table.HeaderCell>Sport</Table.HeaderCell>
+                                        </Table.Row>
                                     </Table.Header>
                                     <Table.Body>
-                                        <TableBody>
-                                            {
+                                        {
 
-                                                events.forEach(event => {
-                                                    return (
-                                                        <TableRow>
-                                                            <Label>event.eventName</Label>
-                                                        </TableRow>
-                                                    );
-                                                })
-                                            }
-                                        </TableBody>
+                                            events.map(event => {
+                                                return (
+                                                    <Table.Row>
+                                                        <Table.Cell>{event.eventName}</Table.Cell>
+                                                        <Table.Cell>{parseUtcDate(event.eventDate)}</Table.Cell>
+                                                        <Table.Cell>{event.league.name}</Table.Cell>
+                                                        <Table.Cell>{event.sport.name}</Table.Cell>
+                                                    </Table.Row>
+                                                );
+                                            })
+                                        }
                                     </Table.Body>
                                 </Table>
                             </Segment>
