64feead2006597caa9c7e03ebe457b05e8f8db64
Alexander Ivanov <alexander_ivanov@mail.com>
2019-06-16 12:07:52 +0300

    home.js table

--- source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/components/Home/Home.js
+++ source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Table, Segment, Image } from "semantic-ui-react";
+import { Grid, Table, TableHeader, TableHeaderCell, TableBody, TableRow, TableCell, Label, Segment, Image } from "semantic-ui-react";
 import "./Home.css";
 
 export class Home extends Component {
@@ -10,44 +10,51 @@ export class Home extends Component {
 
     static displayName = Home.name;
 
-  render() {
-      const events = this.props.events;
-      const sports = this.props.sports;
-    return (
-      <Grid columns={3} className="home-grid">
-        {events.length>0 ? 
-          <Grid.Row className="home-grid-row">
-            <Grid.Column width={3} className="home-grid-sports">
-              <Segment className="home-grid-sports" />
-            </Grid.Column>
-            <Grid.Column width={11} className="home-grid-main">
-              <Segment className="home-grid-main" />
-            </Grid.Column>
-            <Grid.Column width={2} className="home-grid-ads">
-              <Image src={require("./../../public/sportsbanner.jpg")} />
-            </Grid.Column>
-          </Grid.Row>
-         : null}
-      </Grid>
-    );
-  }
+    render() {
+        const events = this.props.events;
+        const sports = this.props.sports;
+        return (
+            <Grid columns={3} className="home-grid">
+                {events.length > 0 ?
+                    <Grid.Row className="home-grid-row">
+                        <Grid.Column width={3} className="home-grid-sports">
+                            <Segment className="home-grid-sports" />
+                        </Grid.Column>
+                        <Grid.Column width={11} className="home-grid-main">
+                            <Segment className="home-grid-main" >
+                                <Table>
+                                    <Table.Header>
+                                        <TableHeader>
+                                            <TableHeaderCell>Event Name</TableHeaderCell>
+                                            <TableHeaderCell>Date</TableHeaderCell>
+                                            <TableHeaderCell>League</TableHeaderCell>
+                                            <TableHeaderCell>Sport</TableHeaderCell>
+                                        </TableHeader>
+                                    </Table.Header>
+                                    <Table.Body>
+                                        <TableBody>
+                                            {
+
+                                                events.forEach(event => {
+                                                    return (
+                                                        <TableRow>
+                                                            <Label>event.eventName</Label>
+                                                        </TableRow>
+                                                    );
+                                                })
+                                            }
+                                        </TableBody>
+                                    </Table.Body>
+                                </Table>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column width={2} className="home-grid-ads">
+                            <Image src={require("./../../public/sportsbanner.jpg")} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    : null}
+            </Grid>
+        );
+    }
 }
 
-/*
- //{
-                //    events.length > 0 ?
-                //    <Grid.Row className="home-grid-row">
-                //        <Grid.Column width={3} className="home-grid-sports">
-                //            <Segment className="home-grid-sports" />
-                //        </Grid.Column>
-                //        <Grid.Column width={11} className="home-grid-main">
-                //            <Segment className="home-grid-main" />
-                //        </Grid.Column>
-                //        <Grid.Column width={2} className="home-grid-ads">
-                //            <Image src={require("./../../public/sportsbanner.jpg")} />
-                //        </Grid.Column>
-                //    </Grid.Row>
-                //    :
-                //    null
-                //}
- */
