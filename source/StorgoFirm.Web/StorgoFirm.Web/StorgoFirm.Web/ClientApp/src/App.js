9dc1301f33ac9c2c2b539d5a014622f0e6d119b7
bokoto000 <bobi_velk@abv.bg>
2019-06-16 09:39:24 +0300

    Search Bar

--- source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/App.js
+++ source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout/Layout";
+import { Home } from "./components/Home/Home";
+import { FetchData } from "./components/FetchData/FetchData";
+import { Counter } from "./components/Counter/Counter";
+import './App.css';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <div className="app-window">
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+        <Layout>
+          <Route exact path="/" component={Home} />
+          <Route path="/counter" component={Counter} />
+          <Route path="/fetch-data" component={FetchData} />
+        </Layout>
+      </div>
     );
   }
 }
---
+++ source/StorgoFirm.Web/StorgoFirm.Web/StorgoFirm.Web/ClientApp/src/components/NavMenu/NavMenu.js
@@ -0,0 +1,186 @@
+import React from "react";
+import {
+  Button,
+  Container,
+  Menu,
+  Segment,
+  Visibility,
+  Image,
+  Popup
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import "./NavMenu.css";
+
+export default class NavMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user,
+      authenticating: props.authenticating
+    };
+  }
+
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
+
+  render() {
+    const fixed = this.state.fixed;
+    const authenticating = this.state.authenticating;
+    const user = this.state.user;
+    if (authenticating) {
+      return (
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+          className="dekstop-header"
+        >
+          <Segment
+            inverted
+            textAlign="center"
+            vertical
+            className="dekstopheader"
+          >
+            <Menu
+              fixed={fixed ? "top" : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size="large"
+              className="dekstopheader-menu"
+              style={{ padding: "0em 0em" }}
+            >
+              <Container>
+                <Menu.Item as={Link} to="/" style={{ padding: "0em 0em" }} />
+              </Container>
+            </Menu>
+          </Segment>
+        </Visibility>
+      );
+    } else {
+      if (user) {
+        return (
+          <Visibility
+            once={false}
+            onBottomPassed={this.showFixedMenu}
+            onBottomPassedReverse={this.hideFixedMenu}
+            className="dekstop-header"
+          >
+            <Segment
+              inverted
+              textAlign="center"
+              vertical
+              className="dekstopheader"
+            >
+              <Menu
+                fixed={fixed ? "top" : null}
+                inverted={!fixed}
+                pointing={!fixed}
+                secondary={!fixed}
+                size="large"
+                className="dekstopheader-menu"
+                style={{ padding: "0em 0em" }}
+              >
+                <Container>
+                  <Menu.Item as={Link} to="/" style={{ padding: "0em 0em" }} />
+                  <Menu.Item position="right">
+                    <Button
+                      as={Link}
+                      to="/timeline"
+                      inverted={fixed}
+                      style={{ marginLeft: "0.5em" }}
+                      className="dekstopheader-button"
+                      color="gray"
+                    >
+                      Дневник
+                    </Button>
+                    <Button
+                      as={Link}
+                      to="/profile"
+                      inverted={fixed}
+                      style={{ marginLeft: "0.5em" }}
+                      className="dekstopheader-button"
+                      color="gray"
+                    >
+                      Профил
+                    </Button>
+                    <Button
+                      onClick={this.handleSignOut}
+                      inverted={fixed}
+                      style={{ marginLeft: "0.5em" }}
+                      className="dekstopheader-button"
+                      color="gray"
+                    >
+                      Изход
+                    </Button>
+                  </Menu.Item>
+                </Container>
+              </Menu>
+            </Segment>
+          </Visibility>
+        );
+      } else {
+        return (
+          <Visibility
+            once={false}
+            onBottomPassed={this.showFixedMenu}
+            onBottomPassedReverse={this.hideFixedMenu}
+            className="dekstop-header"
+          >
+            <Segment
+              inverted
+              textAlign="center"
+              vertical
+              className="dekstopheader"
+            >
+              <Menu
+                fixed={fixed ? "top" : null}
+                inverted={!fixed}
+                pointing={!fixed}
+                secondary={!fixed}
+                size="large"
+                className="dekstopheader-menu"
+                style={{ padding: "0em 0em" }}
+              >
+                <Container>
+                  <Menu.Item as={Link} to="/" style={{ padding: "0em 0em" }} />
+                  <Menu.Item>
+                    <SearchBar />
+                  </Menu.Item>
+                  <Menu.Item position="right" style={{ padding: "1em 0em" }}>
+                    <Popup
+                      trigger={
+                        <Button
+                          inverted={fixed}
+                          primary={fixed}
+                          className="dekstopheader-button"
+                          color="gray"
+                        >
+                          Вход
+                        </Button>
+                      }
+                      basic
+                      on="click"
+                    />
+                    <Button
+                      as={Link}
+                      to="/signup"
+                      inverted={fixed}
+                      primary={fixed}
+                      style={{ marginLeft: "0.5em" }}
+                      className="dekstopheader-button"
+                      color="gray"
+                    >
+                      Регистрация
+                    </Button>
+                  </Menu.Item>
+                </Container>
+              </Menu>
+            </Segment>
+          </Visibility>
+        );
+      }
+    }
+  }
+}
